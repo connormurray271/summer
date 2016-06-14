688db05a03605ee635798643c6c5fef5ad09d597
connormurray271 <connormurray@dhcp-10-105-7-25.wireless.northwestern.private>
2016-06-14 17:36:23 -0500

    doesn't recognize

--- CDC/app/src/main/java/com/example/connormurray/cdc/MainActivity.java
+++ CDC/app/src/main/java/com/example/connormurray/cdc/MainActivity.java
@@ -1,43 +1,55 @@
 package com.example.connormurray.cdc;
 
 import android.content.Context;
-import android.hardware.usb.UsbDevice;
+import android.content.Intent;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
-import com.felhr.usbserial.UsbSerialDevice;
-import com.felhr.usbserial.UsbSerialInterface;
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.util.SerialInputOutputManager;
 
-import org.w3c.dom.Text;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static UsbSerialPort sPort = null;
+    private final ExecutorService mExecutor = Executors.newSingleThreadExecutor();
+
+    private SerialInputOutputManager mSerialIoManager;
+
+    private final SerialInputOutputManager.Listener mListener =
+            new SerialInputOutputManager.Listener() {
+                @Override
+                public void onRunError(Exception e) {
+//                    Log.d(TAG, "Runner stopped.");
+                }
+
+                @Override
+                public void onNewData(final byte[] data) {
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+//                            MainActivity.this.updateReceivedData(data);
+                        }
+                    });
+                }
+            };
+
+
     SeekBar slider;
     TextView sliderText;
-    TextView comText;
+    TextView picText;
+    TextView deviceCheck;
+    TextView connectionCheck;
 
-    int progressChanged;
 
-    UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
-    UsbDevice device;
-    UsbDeviceConnection connection;
-
-    private UsbSerialInterface.UsbReadCallback mCallback = new UsbSerialInterface.UsbReadCallback()
-    {
-        @Override
-        public void onReceivedData(byte[] arg0){
-            comText.setText(""+arg0);
-
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,91 +57,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         slider = (SeekBar) findViewById(R.id.slider);
-
         sliderText = (TextView) findViewById(R.id.sliderText);
-        comText = (TextView) findViewById(R.id.comText);
-
-        setMyControlListener();
-        findSerialPortDevice();
-
-
+        picText = (TextView) findViewById(R.id.picText);
+        deviceCheck = (TextView) findViewById(R.id.deviceCheck);
+        connectionCheck = (TextView) findViewById(R.id.connectionCheck);
     }
 
     @Override
-    protected  void onResume(){
+    protected void onResume() {
         super.onResume();
-
-        UsbSerialDevice serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
-        if(serialPort != null)
-        {
-            if(serialPort.open())
-            {
-                // Devices are opened with default values, Usually 9600,8,1,None,OFF
-                // CDC driver default values 115200,8,1,None,OFF
-                serialPort.setBaudRate(115200);
-                serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
-                serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
-                serialPort.setParity(UsbSerialInterface.PARITY_NONE);
-                serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
-                serialPort.read(mCallback);
-
-                String sendString = String.valueOf(100) + '\n';
-                serialPort.write(sendString.getBytes());
-            }else
-            {
-                // Serial port could not be opened, maybe an I/O error or it CDC driver was chosen it does not really fit
-            }
-        }else
-        {
-            // No driver for given device, even generic CDC driver could not be loaded
-        }
-    }
-
-
-    private void setMyControlListener(){
-        slider.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                progressChanged = i;
-                sliderText.setText("Value: " +i);
+        if (sPort == null){
+            deviceCheck.setText("No device");
+        }else{
+            final UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+            UsbDeviceConnection connection = usbManager.openDevice(sPort.getDriver().getDevice());
+            if (connection == null){
+                connectionCheck.setText("Opening device failed");
+                return;
             }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
+            try{
+                sPort.open(connection);
+                sPort.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+            } catch (IOException e) {
+                connectionCheck.setText("Serial port didn't open");
+                try{
+                    sPort.close();
+                } catch (IOException e1) {
 
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-    }
-
-    private void findSerialPortDevice() {
-        HashMap<String, UsbDevice> usbDevices = usbManager.getDeviceList();
-        if(!usbDevices.isEmpty())
-        {
-            boolean keep = true;
-            for(Map.Entry<String, UsbDevice> entry : usbDevices.entrySet())
-            {
-                device = entry.getValue();
-                int deviceVID = device.getVendorId();
-                int devicePID = device.getProductId();
-                if(deviceVID != 0x1d6b || (devicePID != 0x0001 || devicePID != 0x0002 || devicePID != 0x0003))
-                {
-                    // We are supposing here there is only one device connected and it is our serial device
-                    connection = usbManager.openDevice(device);
-                    keep = false;
-                }else
-                {
-                    connection = null;
-                    device = null;
                 }
-
-                if(!keep)
-                    break;
+                sPort = null;
+                return;
             }
         }
+    }
+
+    static void show(Context context, UsbSerialPort port) {
+        sPort = port;
+        final Intent intent = new Intent(context, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NO_HISTORY);
+        context.startActivity(intent);
     }
 }
--- cdcapp/app/src/main/java/com/example/connormurray/cdcapp/MainActivity.java
+++ cdcapp/app/src/main/java/com/example/connormurray/cdcapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.connormurray.cdcapp;
 
+import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,48 +11,76 @@ import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.felhr.usbserial.CDCSerialDevice;
 import com.felhr.usbserial.UsbSerialDevice;
 import com.felhr.usbserial.UsbSerialInterface;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    public final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
 
     SeekBar slider;
     TextView sliderText;
     TextView picText;
+    TextView connectionText;
+    TextView checkText;
+    TextView dataText;
+    TextView readText;
+    TextView checkText2;
+    TextView checkText3;
+    TextView checkText4;
     int progress;
     UsbManager usbManager;
     UsbDevice device;
     UsbDeviceConnection connection;
     UsbSerialDevice serialPort;
+    int i = 0;
 
     private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
-            if(serialPort != null){
-                if(serialPort.open()){
-                    serialPort.setBaudRate(9600);
-                    serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
-                    serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
-                    serialPort.setParity(UsbSerialInterface.PARITY_NONE);
-                    serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
-                    serialPort.read(mCallback);
+            if (intent.getAction().equals(ACTION_USB_PERMISSION)) {
+                boolean granted = intent.getExtras().getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
+                if (granted) {
+                    connection = usbManager.openDevice(device);
+//                    checkText4.setText("Granted" + serialPort.open());
+                    serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
+                    checkText4.setText("Granted");
+                    if(serialPort != null){
+                        checkText2.setText("Serial port != null");
+                        if(serialPort.open()){
+                            checkText3.setText("Serialport.open");
+                            serialPort.setBaudRate(9600);
+                            serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
+                            serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
+                            serialPort.setParity(UsbSerialInterface.PARITY_NONE);
+                            serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+                            serialPort.read(mCallback);
+                            readText.setText("Reading");
 
+                        }else{
+                            checkText3.setText("Serial is not open");
+                        }
+                    }else{
+                        checkText2.setText("Serial port is null (bad)");
+
+                    }
                 }else{
-
+                    checkText4.setText("Not granted");
                 }
-            }else{
-
             }
+
         }
     };
 
@@ -63,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
                 data = new String(bytes, "UTF-8");
                 data.concat("/n");
                 picText.setText(data);
+                dataText.setText("Getting data");
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
@@ -77,32 +108,47 @@ public class MainActivity extends AppCompatActivity {
         slider = (SeekBar) findViewById(R.id.slider);
         sliderText = (TextView) findViewById(R.id.sliderText);
         picText = (TextView) findViewById(R.id.picText);
+        connectionText = (TextView) findViewById(R.id.connectionText);
+        checkText = (TextView) findViewById(R.id.checkText);
+        dataText = (TextView) findViewById(R.id.dataText);
+        readText = (TextView) findViewById(R.id.readText);
         usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        checkText2 = (TextView) findViewById(R.id.checkText2);
+        checkText3 = (TextView) findViewById(R.id.checkText3);
+        checkText4 = (TextView) findViewById(R.id.checkText4);
 
         Controller();
 
         IntentFilter filter = new IntentFilter();
-
+        filter.addAction(ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
         registerReceiver(broadcastReceiver, filter);
 
         HashMap<String, UsbDevice> usbDevices = usbManager.getDeviceList();
         if(!usbDevices.isEmpty()){
+            checkText.setText("USB connected");
             boolean keep = true;
             for (Map.Entry<String, UsbDevice> entry : usbDevices.entrySet()){
                 device = entry.getValue();
                 int deviceVID = device.getVendorId();
                 int devicePID = device.getProductId();
                 if(deviceVID != 0x1d6b || (devicePID != 0x0001 || devicePID != 0x0002 || devicePID != 0x0003)){
-                    connection = usbManager.openDevice(device);
+                    PendingIntent pi = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+                    usbManager.requestPermission(device,pi);
                     keep = false;
+                    connectionText.setText("Accepted");
                 }else{
                     connection = null;
                     device = null;
+                    connectionText.setText("Not Accepted");
                 }
                 if(!keep) {
                     break;
                 }
             }
+        }else{
+            checkText.setText("USB empty");
         }
     }
 
@@ -113,7 +159,12 @@ public class MainActivity extends AppCompatActivity {
                 progress = i;
                 sliderText.setText("Slider: " +progress);
                 String sendString = String.valueOf(i) + '\n';
-                serialPort.write(sendString.getBytes());
+                try{
+                    serialPort.write(sendString.getBytes());
+                }
+                catch (Throwable e){
+                    e.printStackTrace();
+                }
             }
 
             @Override
